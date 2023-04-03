1ec29eeea89bd5a8955f069ad995718e36843294
Maksim Lin <maks@manichord.com>
2023-04-03 12:50:53 +1000

    move each example into separate main file
    default square window to make fixing gradient shader orientation easier

---
+++ lib/main_helloworld.dart
@@ -0,0 +1,84 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Shaders',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: FutureBuilder<FragmentShader>(
+                  future: _load(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final shader = snapshot.data!;
+                      return CustomPaint(
+                        painter: ShaderPainter(shader),
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ShaderPainter extends CustomPainter {
+  final FragmentShader shader;
+
+  ShaderPainter(this.shader);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawRect(
+      Rect.fromLTWH(0, 0, size.width, size.height),
+      Paint()..shader = shader,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+Future<FragmentShader> _load() async {
+  FragmentProgram program = await FragmentProgram.fromAsset('shaders/hellow.frag');
+  return program.fragmentShader();
+}
